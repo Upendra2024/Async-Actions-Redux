8c20797380cd8b434b75f9e08fc0f9777b6e7cf0
Upendra2024 <92069507+Upendra2024@users.noreply.github.com>
2022-10-12 14:01:24 +0530

    2 files modified, 7 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import {UserContainer} from './components/UserContainer';
+
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     
+      <h1>Hello Redux </h1>
+      <UserContainer/>
     </div>
   );
 }
